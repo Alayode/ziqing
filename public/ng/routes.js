bd95f0a04c3a34445f4c2be35b6ffb7621ebeae8
Chris Samuel <ksamuel.chris@icloud.com>
2015-09-29 16:36:51 -0400

    last commit didn't resolve issue, updated all ng modules in angular module object

--- public/ng/routes.js
+++ public/ng/routes.js
@@ -16,49 +16,50 @@ sub page vies that we show or hide from the user, as appropriate.
 // for  a given URL that the browser points to, Angular should load and display a Template, and instantiate
 // a controller to provide context for the template.
 //
-angular.module('MyApp', [ 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-    .config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', {
-                templateUrl: 'layouts/home.html',
-                controller: 'MainCtrl'
-            })
-            .when('/shows/:id', {
-                templateUrl: 'layouts/detail.html',
-                controller: 'DetailCtrl'
-            })
-            .when('/login', {
-                templateUrl: 'layouts/login.html',
-                controller: 'LoginCtrl'
-            })
-            .when('/signup', {
-                templateUrl: 'layouts/signup.html',
-                controller: 'SignupCtrl'
-            })
-            .when('/add', {
-                templateUrl: 'layouts/add.html',
-                controller: 'AddCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .config(function ($httpProvider) {
-        $httpProvider.interceptors.push(function ($rootScope, $q, $window, $location) {
-            return {
-                request: function(config) {
-                    if ($window.localStorage.token) {
-                        config.headers.Authorization = 'Bearer ' + $window.localStorage.token;
-                    }
-                    return config;
-                },
-                responseError: function(response) {
-                    if (response.status === 401 || response.status === 403) {
-                        $location.path('/login');
-                    }
-                    return $q.reject(response);
-                }
-            }
-        });
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'ngAnimate', 'mgcrea.ngStrap'])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'layouts/home.html',
+        controller: 'MainCtrl'
+      })
+      .when('/shows/:id', {
+        templateUrl: 'layouts/detail.html',
+        controller: 'DetailCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'layouts/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'layouts/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/add', {
+        templateUrl: 'layouts/add.html',
+        controller: 'AddCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push(function ($rootScope, $q, $window, $location) {
+      return {
+        request: function(config) {
+          if ($window.localStorage.token) {
+            config.headers.Authorization = 'Bearer ' + $window.localStorage.token;
+          }
+          return config;
+        },
+        responseError: function(response) {
+          if (response.status === 401 || response.status === 403) {
+            $location.path('/login');
+          }
+          return $q.reject(response);
+        }
+      }
     });
+  });
