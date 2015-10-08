fc54fac8d9ed746234f00daa592ef913613550cb
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-08 13:00:23 -0400

    added comments to the program explaining how it is done

--- jsexamples/localvsglobal.js
+++ jsexamples/localvsglobal.js
@@ -1,6 +1,9 @@
 
-function getArea(width,height){
-    var area = width * height;
+
+    //we declare a function called getArea
+function getArea(width,height){// we pass in two variables called width and height.
+    var area = width * height; // we will multiply width by height and store it in a declared variable called area
+    //stops the execution of the function and returns the value of  function
     return area;
 }
 
