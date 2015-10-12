9a93c04ff540e3d5fb7da9e18bc028ff55062673
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-12 10:34:16 -0400

    created the newOperatorExample.js

---
+++ jsexamples/newOperatorExample.js
@@ -0,0 +1,25 @@
+
+
+/*
+* Chris Samuel
+* ksamuel.chris@icloud.com
+*
+*
+*Filename: newOperatorExample.js
+*
+* Description
+*
+* I never really understood the new operator very well before I always thought
+* It was just used for creating new objects. There is alot more behind this JavaScript
+* Keyword.
+*
+*  new constructor[ ([ arguments ]) ]
+*
+*
+*   constructor  - a function that specifies a type
+*   arguments    - a list of values that the constructor will be called with.
+*   * */
+
+/* The new operator creates an instance of a user-defined object type or one of the built-in object types that has a constructor function */
+
+
