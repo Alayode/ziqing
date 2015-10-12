7c4b55d55b6a0dae0198ed006550ce81ad43c579
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-12 10:52:55 -0400

    Added an example fo the new Operator using the Date object

--- jsexamples/newOperatorExample.js
+++ jsexamples/newOperatorExample.js
@@ -24,4 +24,7 @@
 
 
 
-        var d;
+        //var d =  date(); // this is wrong   will return undefined
+        //var d =  new date(); // almost but you forgot one importatnt thing.  the date() could be a non built in function but it has no capital letter not defined
+        var d =  new Date(); // the Date() object is a constructor in which it is always expressed with a Capital first letter.
+                console.log(d); //Mon Oct 12 2015 10:50:50 GMT-0400 (EDT) it will be the current date.
