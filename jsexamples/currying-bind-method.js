375d58752e82f3696f66fd302087464bca1b73fe
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-21 09:47:19 -0400

    added deeper explaination of binding and currying in javascript

--- jsexamples/currying-bind-method.js
+++ jsexamples/currying-bind-method.js
@@ -13,7 +13,13 @@ ksamuel.chris@gmail.com
 */
 
 
-function f(y) { return this.x + y } //This function needs to be bound
-var o = { x:1 };
-
-  console.log(f(y));// Error y is undefined
+function f(y) {
+  return this.x + y
+}; //This function needs to be bound
+  // if called this function right now we will end up with x being undefined
+var o = { x : 1 };
+console.log(o);//{x:1};
+console.log(f());//NaN
+console.log(f.bind(o)); //[Function: bound f]
+var g = f.bind(o); // calling g(x) invokes o.f(x)
+console.log(g(1));
